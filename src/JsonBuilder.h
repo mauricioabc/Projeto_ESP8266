bb5068f3dca1f78c9b0d44c2818b365d8b8514e9
Maurício <mauricioarruda660@gmail.com>
2024-03-30 15:37:01 -0300

    :boom: fix: Removing unused tech from JsonBuilder
    - Ajuste de trecho não usado no JsonBuilder.

--- src/JsonBuilder.h
+++ src/JsonBuilder.h
@@ -4,9 +4,6 @@
 #include <cstddef>
 
 class JsonBuilder {
-private:
-    int pin; // Pino do Led
-
 public:
     JsonBuilder(); // Construtor
     void JsonMessageBuilder(const char* tag, float value, char* jsonBuffer, size_t bufferSize);
